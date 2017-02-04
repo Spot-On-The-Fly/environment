3574037dca6d1adcc699ad51db389acf75585317
Jack Pearson <Jack Pearson>
2017-02-04 13:29:02 +0000

    -a

--- test_server/tst.js
+++ test_server/tst.js
@@ -1,4 +1,15 @@
-//Ic hack test
-console.log('hello world');
+function user(name, genres, artists, songs){
+  this.user = name;
+  this.genres = genres;
+  this.artists = artists;
+  this.songs = songs;
+}
 
-//
+var michael = new user(
+
+  "michael the dickhead",
+  "edm",
+  "shit",
+  "oh no plz"
+);
+console.log(michael);
